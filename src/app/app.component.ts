cb364310cc126a448b36c1a7c22f7be7762a159f
Ulises Miranda <ulimirandachavez@hotmail.com>
2022-07-14 20:39:57 -0500

    Calculadora Basica Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadoraBasica';
+  title:string = 'Aplicación de Calculadora';
+  operandoA = 0
+  operandoB = 0
+  resultado?:number
+
+  sumar() {
+    this.resultado = this.operandoA + this.operandoB;
+  }
 }
